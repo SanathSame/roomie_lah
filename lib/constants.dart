344e18f618c4d99739fc897e8dfb29d2f6eaa0a4
Aks <tayalaks2001@gmail.com>
2022-03-03 21:09:03 +0800

    Constants and appBar added

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0x5DB075);
+
+final TextStyle kSmallText =
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.white);
+final TextStyle kMediumText =
+    TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white);
+final TextStyle kLargeText =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white);
+final TextStyle kLargeBoldText =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: Colors.white);
