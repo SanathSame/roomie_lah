70f6c01868b4e3106c26cde7232cfd6b93d273aa
Aks <tayalaks2001@gmail.com>
2022-03-15 23:31:33 +0800

    Beautification of chat list screen

--- lib/widgets/chat_preview.dart
+++ lib/widgets/chat_preview.dart
@@ -2,25 +2,18 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-
-
-
 // CHANGE OPACITY DEPENDING ON IF THE MESSAGE HAS BEEN READ OR NOT
 // CHANGE THE SIZE OF WIDGETS DEPENDING ON SIZE OF SCREEN
 // CAN ADD A CIRCLE ON THE PROFILE IF THE PERSON IS ONLINE OR NOT
 
-
-
 // ignore: must_be_immutable
 class ChatPreview extends StatelessWidget {
-
   late Image _profilePic;
   late String _name;
   late String _lastMessage;
   late String _time;
 
-
-  ChatPreview(String name, String lastMessage, String time){
+  ChatPreview(String name, String lastMessage, String time) {
     //this._profilePic = profilePic;
     this._name = name;
     this._lastMessage = lastMessage;
@@ -33,33 +26,48 @@ class ChatPreview extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20 * 0.75),
+      padding: EdgeInsets.symmetric(
+          horizontal: width * 0.05, vertical: height * 0.01),
       child: Row(
-        children: <Widget> [
+        children: <Widget>[
           new CircleAvatar(
             backgroundImage: AssetImage('assets/hasbullah.jpeg'),
-            radius: 0.05 * max(width, height),
+            radius: 0.03 * max(width, height),
           ),
           Expanded(
-          child:Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget> [
-                  Text(this._name, style: TextStyle(fontSize: 0.03 * width, fontWeight: FontWeight.w500),),
-                  SizedBox(height: 0.027 * height,),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    this._name,
+                    style: TextStyle(
+                        fontSize: 0.03 * width, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 0.015 * height,
+                  ),
                   Opacity(
                     opacity: 0.64,
-                      child: Text(this._lastMessage, maxLines: 1, overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 0.03 * width),),),
-                  ],
+                    child: Text(
+                      this._lastMessage,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 0.03 * width),
+                    ),
+                  ),
+                ],
               ),
-          ),
+            ),
           ),
           Spacer(),
           Opacity(
               opacity: 0.64,
-              child: Text(this._time, style:
-                TextStyle(fontSize: 0.025 * width),)),
+              child: Text(
+                this._time,
+                style: TextStyle(fontSize: 0.025 * width),
+              )),
         ],
       ),
     );
