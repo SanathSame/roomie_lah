25967679e4dec75484824c06049e2448bbfc9985
Srishti <srishti.arora72@gmail.com>
2022-03-28 19:40:10 +0800

    update algo

--- lib/controllers/MatchingAlgo.py
+++ lib/controllers/MatchingAlgo.py
@@ -15,38 +15,59 @@ def get_recommendations(username):
     # Reading matches data
     matches_db = db.collection('matches')
     matches_docs = matches_db.stream()
+    matched_users = []
     for doc in matches_docs:
-        matches = doc.to_dict()
+        if doc.id == username:
+            matches_dict = doc.to_dict()
+            for i in matches_dict['matches']:
+                matched_users.append(i['username'])
 
     # Reading user data
     users = db.collection('users')
     docs = users.stream()
 
-    all_vectors = []
     all_users_dict = {}
+    try:
+        for doc in docs:
+            usr_dict = doc.to_dict()
+
+            vec = [int(usr_dict['dayPerson']), int(usr_dict['drink']), int(usr_dict['football']), int(usr_dict['movies']),
+                   int(usr_dict['Smoking']), int(usr_dict['vegetarian']), int(usr_dict['party'])]
+            usr_dict['vector'] = vec
+            all_users_dict[usr_dict['username']] = usr_dict
+    except:
+        print(usr_dict['username'], 'exception')
+
+    # Reading user preferences
+    pref = db.collection('preferences')
+    docs = pref.stream()
 
     for doc in docs:
-        usr_dict = doc.to_dict()
-
-        vec = [int(usr_dict['dayPerson']), int(usr_dict['drink']), int(usr_dict['football']), int(usr_dict['movies']),
-               int(usr_dict['Smoking']), int(usr_dict['vegetarian'])]
-        # all_vectors.append(vec)
-        usr_dict['vector'] = vec
-        all_users_dict[usr_dict['username']] = usr_dict
+        pref_dict = doc.to_dict()
+        if pref_dict['username'] == username:
+            my_pref = pref_dict
 
     # Recommendation Algo
-    all_recommendations = {}
     recommendations_score = {}
     # this is a list of users sorted in desc order on the rec score
     recommended_usernames = []
-    my_vector = all_users_dict[username]['vector']
+    try:
+        my_vector = all_users_dict[username]['vector']
+    except:
+        print(username, ' does not exist')
+
     for user in all_users_dict:
         if user == username:
+            continue
+        if user in matched_users:
             continue
         if all_users_dict[user]['gender'] != all_users_dict[username]['gender']:
             continue
         if all_users_dict[user]['university'] != all_users_dict[username]['university']:
             continue
+        for i in my_pref:
+            if my_pref[i] != all_users_dict[user][i]:
+                continue
         recommendations_score[user] = 0
         your_vector = all_users_dict[user]['vector']
         for i in range(len(your_vector)):
@@ -64,6 +85,6 @@ def get_recommendations(username):
     return recommended_usernames
 
 
-# rec, score = get_recommendations('Robin1_')
-# for i in rec:
-#     print(i, ':', score[i])
+rec = get_recommendations('user1')
+for i in rec:
+    print(i)
